965c23205418ca34cb2305a61a0f74cf1aeadfcd
Christoffer Jerkeby <kugghjul@gmail.com>
2020-02-17 21:36:54 +0100

    Added top level search.search function. Wrote rudimentary tests.

--- search/__init__.py
+++ search/__init__.py
@@ -7,7 +7,7 @@ import discogs_client
 
 
 class Conf(configparser.ConfigParser):
-    """A class to store configuration items."""
+    """A class to read and store configuration items."""
 
     def __init__(self, filename):
         super(Conf, self).__init__()
@@ -20,15 +20,18 @@ class Conf(configparser.ConfigParser):
 
     def save(self):
         """Write current config to file and read it again."""
-        self._write(self)
+        self._write()
         self.read(self.filename)
 
 
 def query(catalog_number):
-    """Implement a top level search query for catalog number."""
+    """
+    Search query for catalog number.
+    Return a list of discogs_client.Results.
+    """
     api_conf = Conf("search/api.ini")
     connection = discogs_client.Client(api_conf['client']['user_agent'],
-                                        user_token=api_conf['user']['user_token'])
+                                       user_token=api_conf['user']['user_token'])
     results = connection.search(catalog_number, type='catno')
     return results
 
@@ -47,10 +50,36 @@ def result_filter(search_result):
     return shown_result
 
 
-def test():
+def search(catalog_number):
+    """
+    A top level `catalog_number` search that returns a list of result dicts.
+    Usually catalog numbers are unique but not always hence the returned list.
+    """
+    results = query(catalog_number)
+    result_list = []
+    for result in results:
+        dict_result = vars(result)["data"]
+        result_list.append(result_filter(dict_result))
+    return result_list
+
+
+def test_search():
     """Top level test for this package."""
-    results = query("W-90629")
-    result = results[0]
-    dict_result = vars(result)['data']
-    shown = result_filter(dict_result)
-    print(shown)
+    search("W-90629")
+    return True
+
+
+def test_config():
+    """Test to see if config can read and write."""
+    filter_conf = Conf("search/filter.ini")
+    item = "Test"
+    filter_conf[item] = {} # Create section
+    filter_conf[item][item] = item # Create option
+    filter_conf.save()
+    if filter_conf[item][item] == item:
+        filter_conf.remove_option(item, item)
+        filter_conf.remove_section(item)
+        filter_conf.save()
+        if not filter_conf.has_option(item, item):
+            return True
+    return False
